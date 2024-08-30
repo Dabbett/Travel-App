8288c9093547c1c9b9957e6133b10b6f95a5172b
Dylan Abbett <abbettdylan@gmail.com>
2024-08-30 10:33:17 -0400

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+
+const Navbar = () => {
+  return (
+    <nav className="border-b-2 border-zinc-400 shadow-emerald-300 shadow-md flexBetween max-container padding-container fixed top-0 left-0 right-0 z-30 py-5 px-10 bg-inherit">
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={100}
+          height={50}
+          className="rounded"
+        />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="bg-transparent regular-16 text-slate-500 drop-shadow-md flexCenter cursor-pointer transition-all hover:font-extrabold hover:-translate-y-1 hover:underline hover:shadow-slate-900 focus:-translate-y-3 active:-translate-y-3 active:underline active:text-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
